6def6fa382c1a1d5890ab4a230c68b50e5f987b0
Manu <mcabral@frba.utn.edu.ar>
2023-05-16 02:50:40 -0300

    feat(app): home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 export default function App() {
     return (
-        <div className="flex items-center justify-center w-full h-screen bg-slate-600">
-            <h1 className="text-white">Hi! I'm a React app!</h1>
+        <div className="flex flex-col items-center gap-10 bg-gradient-to-r from-red-400 to-red-900 h-screen">
+            <h1 className="text-4xl font-bold text-white">PDF Summize</h1>
+            <input type="file" />
+            <button className="bg-white text-black rounded-full px-4 py-2 hover:bg-gray-200">
+                Upload PDF
+            </button>
+            <div className="bg-white rounded-lg shadow-lg p-4">
+                <h2 className="text-2xl">Summary</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+                    voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.
+                </p>
+            </div>
         </div>
     )
 }
